8b3ef0769926ff6db16fb26ada7a57cb22c06294
YankovichKirill <kirill.best97@gmail.com>
2022-09-14 20:00:45 +0300

    Save2

---
+++ Example2/Program.cs
@@ -0,0 +1,99 @@
+﻿Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"m = {m}, n = {n}.");
+
+double[,] array = new double[m, n];
+
+CreateArrayDouble(array);
+
+WriteArray(array);
+
+Console.WriteLine();
+
+void CreateArrayDouble(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().NextDouble() * 20 - 10;
+        }
+    }
+}
+
+void WriteArray (double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double alignNumber = Math.Round(array[i, j], 1);
+            Console.Write(alignNumber + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("\nWe take the array from the previous task.\n");
+Console.Write("Enter the position coordinates of the element separated by a comma: ");
+
+string? positionElement = Console.ReadLine();
+positionElement = RemovingSpaces(positionElement);
+int[] position = ParserString(positionElement);
+
+if(position[0] <= m
+&& position[1] <= n
+&& position[0] >= 0
+&& position[1] >= 0)
+{
+    double result = array[position[0] - 1, position[1] - 1];
+    Console.Write($"Element value: {result}");
+}
+else Console.Write($"There is no such element in the array.");
+
+int[] ParserString(string input)
+{
+    int countNumbers = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+            countNumbers++;
+    }
+
+    int[] numbers = new int[countNumbers];
+
+    int numberIndex = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        string subString = string.Empty;
+
+        while (input[i] != ',')
+        {
+            subString += input[i].ToString();
+            if (i >= input.Length - 1)
+                break;
+            i++;    
+        }
+        numbers[numberIndex] = Convert.ToInt32(subString);
+        numberIndex++;
+    }
+
+    return numbers;
+}
+
+string RemovingSpaces (string input)
+{
+    string output = string.Empty;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] != ' ')
+        {
+            output += input[i];
+        }
+    }
+    return output;
+}
